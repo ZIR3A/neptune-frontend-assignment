3f834833b48232c82774121562b34733f0eb557d
ZIR3A <zireavai8@gmail.com>
2021-12-09 09:57:11 +0545

    FirstCommit

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -3,3 +3,4 @@ import type { AppDispatch, RootState } from "./store";
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+//custom hooks
